e44b4047e5bd70509b23b5915c4309e63e27640d
Thomas Getgood <thomas@macroexpanse.com>
2023-04-17 17:43:27 -0400

    I was mistaken about how ggml shards the model.
    It splits each layer across all files. This has the massive disadvantage that it
    makes it impossible to load layers from the file one at a time. I'm sure there's
    some reason for it though.
    
    In the meantime let's get the one file version working and then I'll deal with
    sharding.

--- src/GGMLParser.jl
+++ src/GGMLParser.jl
@@ -33,45 +33,6 @@ struct LayerMeta
     ftype::UInt32 # 1 => Float16 values (mostly)
 end
 
-##### Wrapper to read a split file set as if it were one file
-
-mutable struct FilesReader
-    const filenames::Vector{String}
-    metadata
-    index::UInt
-    stream::IOStream
-end
-
-function reader(fnames)
-    return FilesReader(fnames, (), 1, open(fnames[1]))
-end
-
-function Base.skip(h::FilesReader, count)
-    skip(h.stream, count)
-end
-
-function Base.read!(handle::FilesReader, buff)
-    if eof(handle.stream)
-        close(handle.stream)
-        if handle.index <= length(handle.filenames)
-            handle.index += 1
-            handle.stream = open(handle.filenames[handle.index])
-        else
-            throw(Base.EOFError)
-        end
-    end
-
-    read!(handle.stream, buff)
-end
-
-function Base.close(handle::FilesReader)
-    close(handle.stream)
-end
-
-function Base.eof(h::FilesReader)
-    return h.index == length(h.filenames) && eof(h.stream)
-end
-
 ##### General binary reading helpers
 
 function readbits(handle, type)
@@ -88,11 +49,6 @@ end
 
 ##### Attempt at reading
 
-function ffn_inner_units((; ffn_inner_mulitple, embedding))::Int
-    return ffn_inner_mulitple *
-        floor((2*(4*embedding)/3 + ffn_inner_mulitple - 1)/ffn_inner_mulitple)
-end
-
 function readfilemetadata(model)
 
     version = readbits(model, VersionInfo)
@@ -106,7 +62,14 @@ function readfilemetadata(model)
 
     @assert hparams.f16 == 1 "Currently only supporting Float16."
 
+    println("next file:", version, hparams)
+
     return (version, hparams)
+end
+
+function ffn_inner_units((; ffn_inner_mulitple, embedding))::Int
+    return ffn_inner_mulitple *
+        floor((2*(4*embedding)/3 + ffn_inner_mulitple - 1)/ffn_inner_mulitple)
 end
 
 
@@ -175,12 +138,15 @@ function readlayer(model)
     rank = Vector{UInt32}(undef, lm.dimensions)
     read!(model, rank)
 
+    print("reading layer", lm, rank)
     if lm.namelength > 100
         throw("name is too long, this indicates a parsing error.")
     end
 
     name = readstring(model, lm.namelength)
     eltype = eltypelookup(name)
+
+    println(" ", name)
 
     data = Array{eltype}(undef, rank...)
     read!(model, data)
@@ -208,16 +174,8 @@ function readlayermeta(model)
     return (name, rank, datasize)
 end
 
-"""
-Reads the model stored in the list of `files` passed and returns a tuple
-containing hyperparameters and a list of (name, tensor) pairs.
-
-N.B. The files do not have to be in order, but the order of the tensors returned
-will reflect the order in which the input files were passed.
-REVIEW: Is that true?
-"""
-function readmodel(files)
-    model = reader(files)
+function readfile(file)
+    model = open(file)
 
     # Maximum number of tokens to consider
     # FIXME: Presumably this should not be hardcoded...
@@ -236,6 +194,37 @@ function readmodel(files)
     while !eof(model)
         push!(layers, readlayer(model))
     end
+    return (hparams, layers)
+end
+
+"""
+Reads the model stored in the list of `files` passed and returns a tuple
+containing hyperparameters and a list of (name, tensor) pairs.
+
+N.B. The files do not have to be in order, but the order of the tensors returned
+will reflect the order in which the input files were passed.
+REVIEW: Is that true?
+"""
+function readmodel(files)
+    layers = []
+    hparams = undef
+    for file in files
+        (h, l) = readfile(file)
+        if hparams == undef
+            hparams = h
+        else
+            @assert(
+            h == hparams,
+                "file metadata mismatch! got " * string(h) *
+                " but expected " * stirng(hparams) *
+                ". Aborting."
+            )
+        end
+        push!(layers, l)
+    end
+
+    # TODO: layers are split across files, which is a bizzare way of dealing
+    # with it, but so be it.
     return (hparams, layers)
 end
 
--- src/main.jl
+++ src/main.jl
@@ -2,10 +2,25 @@ if !("./" in LOAD_PATH)
     push!(LOAD_PATH, "./")
 end
 
-import .Tokeniser
-import .GGMLParser
+import Tokeniser
+import GGMLParser
 
+## This is just to remind myself of the API. If I'm not tuning or otherwise
+## retraining, is there any point saving the data to yet another file?
+import BSON
 
-files = ["../models/7B/ggml-model-f16.bin"]
+function save(model, fname)
+    BSON.@save(fname, model)
+end
+
+function load(fname)
+    BSON.@load(fname, model)
+    return model
+end
+
+files = [
+    "../models/13B/ggml-model-f16.bin",
+    "../models/13B/ggml-model-f16.bin.1"
+]
 
 l = GGMLParser.readmodel(files)
